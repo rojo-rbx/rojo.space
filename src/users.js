bce2de2a934c4f7a82300a7eac8e0651f08993e8
Lucien Greathouse <me@lpghatguy.com>
2021-07-14 17:56:54 -0400

    Add Funky Friday to games list

--- src/users.js
+++ src/users.js
@@ -11,84 +11,78 @@ import tribe from "./images/users/tribe.png";
 import wfyb from "./images/users/wfyb.png";
 import tds from "./images/users/tower-defense-simulator.png";
 import rcs from "./images/users/robeats-community-server.png";
+import funkyFriday from "./images/users/funky-friday.png";
 
+// Visits last updated on 2021-07-14
 export default [
   {
     name: "Adopt Me",
     url: "https://www.roblox.com/games/920587237",
     image: adopt,
-    plays: 4613613838, // 2020-03-11
   },
   {
     name: "Jailbreak",
     url: "https://www.roblox.com/games/606849621",
     image: jailbreak,
-    plays: 3433164377, // 2020-03-11
+  },
+  {
+    name: "Funky Friday",
+    url: "https://www.roblox.com/games/6447798030",
+    image: funkyFriday,
   },
   {
     name: "Tower Defense Simulator",
     url: "https://www.roblox.com/games/3260590327",
     image: tds,
-    plays: 322107107, // 2020-04-08
   },
   {
     name: "Whatever Floats Your Boat",
     url: "https://www.roblox.com/games/156220906",
     image: wfyb,
-    plays: 48898279, // 2020-03-11
   },
   {
     name: "Giant Simulator",
     url: "https://www.roblox.com/games/2986677229",
     image: giant,
-    plays: 37317030, // 2020-03-11
   },
   {
     name: "Free Draw 2",
     url: "https://www.roblox.com/games/1547610457",
     image: fd2,
-    plays: 55447603, // 2021-04-26
   },
   {
     name: "Eg!",
     url: "https://www.roblox.com/games/2747834912",
     image: eg,
-    plays: 23326904, // 2020-03-11
   },
   {
     name: "Q-CLASH!",
     url: "https://www.roblox.com/games/2029250188",
     image: qclash,
-    plays: 16710370, // 2020-03-11
   },
   {
     name: "City of Rolantis",
     url: "https://www.roblox.com/games/2056459358",
     image: rolantis,
-    plays: 8773780, // 2020-03-11
   },
   {
     name: "Mount Everest Climbing Roleplay",
     url: "https://www.roblox.com/games/3145447020",
     image: everest,
-    plays: 4731199, // 2020-03-11
   },
   {
     name: "Tribe Simulator",
     url: "https://www.roblox.com/games/1891457100",
     image: tribe,
-    plays: 4553045, // 2020-03-11
   },
   {
     name: "RoBeats Community Server",
     url: "https://www.roblox.com/games/4841678118",
     image: rcs,
-    plays: 4008301, // 2021-05-23
   },
   {
     name: "Heroes' Legacy",
     url: "https://www.roblox.com/games/297639921",
     image: heroes,
-    plays: 124358, // 2020-03-11
   },
 ];
