06d1a70ced22c71a9122df17daa69b7a716fa6ec
OverHash <46231745+OverHash@users.noreply.github.com>
2022-02-28 19:58:59 +1300

    Fix GitHub link under footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -113,7 +113,7 @@ const config = {
             },
             {
               label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
+              href: "https://github.com/rojo-rbx/rojo",
             },
           ],
         },
