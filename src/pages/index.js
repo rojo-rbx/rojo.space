0889a25cae261e6217f4c876ec9ead5a86af41ec
Dave Brown <github@railworks2.com>
2022-04-04 09:33:21 +0100

    Fix homepage description and title (#67)
    Simple fix. Hopefully should make embedding look more professionals.

--- src/pages/index.js
+++ src/pages/index.js
@@ -103,8 +103,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={"Homepage"}
+      description="Rojo enables Roblox developers to use professional-grade software engineering tools"
     >
       <main>
         <Splash />
